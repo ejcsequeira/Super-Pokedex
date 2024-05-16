05684866dc7c9c8ea7b4187d5269e86d5249c43b
Edir Sequeira <edirjcs@gmail.com>
2024-05-16 08:43:11 +0100

    done

--- super-pokedex-app/src/components/Navbar.jsx
+++ super-pokedex-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  
+
   return (
     <div className="navbar-container">
       <div className="header-container">
